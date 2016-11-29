a7f4f24b3bb7dc2f1367deecaa490f3b10d79fd5
chrishuie <christopher.huie@galvanize.com>
2016-11-29 08:44:25 -0700

    Added to main excel component to render data and initialize the beginning state of the component

--- app/components/Home.js
+++ app/components/Home.js
@@ -21,18 +21,33 @@ const data = [
 
 
 var Excel = React.createClass ({
+  displayName: 'Excel',
+  getInitialState: function () {
+    return {data: this.props.initialData};
+  },
   render: function () {
     return (
       React.DOM.table(null,
         React.DOM.thead(null,
           React.DOM.tr(null,
-            this.props.headers.map(function(title){
-              return React.DOM.th(null,title);
+            this.props.headers.map(function(title, idx){
+              return React.DOM.th({key: idx},title);
             })
           )
+        ),
+        React.DOM.tbody(null,
+          this.state.data.map(function(row, idx) {
+            return (
+              React.DOM.tr ({key: idx},
+                row.map(function(cell, idx) {
+                  return React.DOM.td({key: idx}, cell);
+                })
+              )
+            );
+          })
         )
       )
-    )
+    );
   }
 });
 
